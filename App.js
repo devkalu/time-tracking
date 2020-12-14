5995200612bb3954cd6f646eddc7008c116cf8d3
Chibueze Kalu <kalu.chibueze@hotmail.com>
2020-12-14 21:25:17 +0100

    additional information into Root file

--- App.js
+++ App.js
@@ -6,10 +6,21 @@ import ToggleableTimmerForm from "./components/ToggleableTimerForm";
 
 export default class App extends React.Component {
   render() {
-    <View>
-      <View>
-        <Text style={}>Timers</Text>
+    <View style={styles.appContainer}>
+      <View style={styles.titleContainer}>
+        <Text style={styles.title}>Timers</Text>
       </View>
+      <ScrollView style={styles.timerList}>
+        <ToggleableTimmerForm isOpen={false} />
+        <EditableTimer
+          id="1"
+          title="Mow the lawn"
+          project="House Chores"
+          elapsed="8986300"
+          isRunning
+        />
+        <EditableTimer />
+      </ScrollView>
     </View>;
   }
 }
