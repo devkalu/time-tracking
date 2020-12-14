09cf29839b5d38139f25e3aff6d640c8d4e226b4
Chibueze Kalu <kalu.chibueze@hotmail.com>
2020-12-14 21:20:33 +0100

    initial commit

--- App.js
+++ App.js
@@ -1,21 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, View, ScrollView, Text } from "react-native";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import EditableTimer from "./components/EditableTimer";
+import ToggleableTimmerForm from "./components/ToggleableTimerForm";
+
+export default class App extends React.Component {
+  render() {
+    <View>
+      <View>
+        <Text style={}>Timers</Text>
+      </View>
+    </View>;
+  }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const styles = StyleSheet.create({});
